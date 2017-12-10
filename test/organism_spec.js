7ba5772bb6b4676e146b9ee945ee3f637a411008
Tommy <tom@nebulousdog.com>
2017-12-09 16:26:48 -0800

    add a basic plant collection initialization test

--- test/organism_spec.js
+++ test/organism_spec.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 import Organism from 'organism';
 
 describe('Organism', function() {
-  describe('#constructor()', function() {
+  describe('#constructor', function() {
     it('should be assumed to be alive', function() {
       const testOrganism = new Organism();
       assert.isTrue(testOrganism.alive);
--- test/plant_collection_spec.js
+++ test/plant_collection_spec.js
@@ -1,1 +1,25 @@
 import PlantCollection from 'plant_collection';
+
+const numSamplePlans = 3;
+const samplePlant = {
+  alive: true,
+};
+const sampleJSON = ko.toJSON((new Array(numSamplePlans)).fill(samplePlant));
+
+describe('PlantCollection', function() {
+  let plantCollection;
+
+  context('#constructor', function() {
+    it('with no params', function() {
+      plantCollection = new PlantCollection();
+      expect(plantCollection.build.calledOnce).to.be.true;
+      expect(plantCollection.sortedPlants()).to.have.lengthof(0);
+    });
+
+    it('with some params', function() {
+      plantCollection = new PlantCollection(sampleJSON);
+      expect(plantCollection.build.calledOnce).to.be.true;
+      expect(plantCollection.sortedPlants()).to.have.lengthof(numSamplePlans);
+    });
+  });
+});
