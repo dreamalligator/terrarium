2d67334b1e02d09e4cec235a44da65e3eb4761b4
Tommy <tom@nebulousdog.com>
2017-12-11 02:12:45 -0800

    add babel deps back in

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,30 @@
 import json from 'rollup-plugin-json';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 
 const defaultConfig = {
   input: './src/main.js',
   name: 'terrarium',
   output: {
     file: './bundle.js',
-    format: 'iife',
+    format: 'umd',
   },
   plugins: [
+    babel({
+      exclude: './node_modules/**',
+    }),
     resolve({
       browser: true,
-      jsnext: true,
-      main: true,
+      // jsnext: true,
+      // main: true,
     }),
     json({
       exclude: ['./node_modules/**'],
       preferConst: true,
     }),
     commonjs({
-      sourceMap: false,
+      // sourceMap: false,
     })
   ],
 };
