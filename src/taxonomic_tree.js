98da78d9812f9998e2918c62c2c8fc87076d2468
Tommy <tom@nebulousdog.com>
2018-04-22 15:37:22 -0700

    fix a couple errors

--- src/taxonomic_tree.js
+++ src/taxonomic_tree.js
@@ -4,7 +4,7 @@ export default class TaxonomicTree {
   }
 
   /**
-   * @return {object[]} all of the raw plant species data
+   * @return {Object[]} all of the raw plant species data
    */
   get rawData() {
     return this._rawData;
@@ -16,7 +16,7 @@ export default class TaxonomicTree {
 
   /**
    * NOTE: this doesn't account for changes in the raw data. Will have to restart app atm to get changes.
-   * @return {string[]} a sorted list of the taxonomic names that we already have data for.
+   * @return {String[]} a sorted list of the taxonomic names that we already have data for.
    */
   validTaxons() {
     if (this._validTaxonNames)
@@ -28,8 +28,8 @@ export default class TaxonomicTree {
 
   /**
    * Based on the preferred order of database ids, choose the first based on taxonomic name
-   * @param {string} taxon taxon
-   * @return {string} database id
+   * @param {String} taxon taxon
+   * @return {String} database id
    */
   getPreferredDatabaseId(taxon) {
     const dbIds = this.taxonomyInfo(taxon).databases;
@@ -37,8 +37,11 @@ export default class TaxonomicTree {
     // TODO: sort by preferred
   }
 
+  /**
+   * @param {String} taxon taxon
+   * @return {Object} info
+   */
   taxonInfoLookup(taxon) {
-    console.log(this.rawData);
     if (this._lookupByTaxon === undefined)
       this._lookupByTaxon = {}; // raw data grouped by taxon
     return this._lookupByTaxon[taxon];
--- src/terrarium.js
+++ src/terrarium.js
@@ -13,17 +13,16 @@ class TerrariumApp {
   constructor() {
     // based on the plants i have in my collection that are _alive_, build the base PlantCollection.
     this.plantCollection = new PlantCollection(rawPlantData);
-    console.log(this.plantCollection.plants[0]);
+
     // this first builds from the data we already have
     this.taxonomyTree = new TaxonomicTree(rawTaxonomyData);
 
     // TODO: move into PlantCollection?
     this.plantCollection.plants.forEach((plant) => {
-      const taxonInfo = this.taxonomyTree.taxonInfoLookup()[plant.taxon];
+      const taxonInfo = this.taxonomyTree.taxonInfoLookup(plant.taxon);
 
       Object.assign(plant, taxonInfo);
     });
-    console.log(this.plantCollection.plants[0]);
 
     // placeholder plant for the add-plant inputs
     this.temporaryPlant = ko.observable({
@@ -32,7 +31,6 @@ class TerrariumApp {
     });
 
     this.temporaryPlant.subscribe((newPlant) => {
-      console.log(newPlant);
       // only suggest an id if nothing is already written
       if (newPlant.id !== null)
         return;
@@ -44,6 +42,10 @@ class TerrariumApp {
     });
   }
 
+  /**
+   * @param {String} taxon taxon
+   * @return {String} a suggested plant id
+   */
   suggestPlantId(taxon) {
     const databaseId = this.taxonomyTree.getPreferredDatabaseId(taxon);
     const n = this.plantCollection.numberOfTaxon() - 1;
